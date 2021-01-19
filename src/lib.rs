900534e895a8eef09623e3674f1e484d1b1efcb6
ValleyKnight <ValleyKnight@protonmail.com>
2021-01-19 08:05:55 -0500

    add check to GPU

--- src/lib.rs
+++ src/lib.rs
@@ -67,12 +67,8 @@ pub fn env(var: &str) -> Option<String> {
     Some(env::var(var).unwrap_or_else(|_| format!("N/A (could not read ${}, are you sure it's set?)", var)))
 }
 
-/// Obtain the name of the GPU, outputs to a string
-pub fn gpu() -> io::Result<String> {
-    let output = Command::new("sh")
-        .args(&["-c", "lspci | grep -I 'VGA\\|Display\\|3D'"])
-        .output()?;
-    let model = String::from_utf8_lossy(&output.stdout)
+fn r#continue(output_check: String) -> io::Result<String> {
+    let model = output_check
         .split(':')
         .collect::<Vec<&str>>()[2]
         .trim()
@@ -85,6 +81,19 @@ pub fn gpu() -> io::Result<String> {
             .replace("\n", ""))
     } else {
         Ok(model.replace("\n", ""))
+    }
+}
+
+/// Obtain the name of the GPU, outputs to a string
+pub fn gpu() -> io::Result<String> {
+    let output = Command::new("sh")
+        .args(&["-c", "lspci | grep -I 'VGA\\|Display\\|3D'"])
+        .output()?;
+    let output_check: String = String::from_utf8_lossy(&output.stdout).to_string();
+    if output_check.is_empty() {
+        Ok("N/A (could not run lspci/grep, make sure they are installed)".to_string())
+    } else {
+        r#continue(output_check)
     }
 }
 
