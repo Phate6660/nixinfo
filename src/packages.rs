3fdbc229e07b34f6cd31e402426343369505855a
Dam-0 <Damien.peters2000@gmail.com>
2021-08-15 22:31:41 -0400

    test

--- src/packages.rs
+++ src/packages.rs
@@ -1,4 +1,11 @@
-pub fn count(output: std::process::Output) -> usize {
+//pub fn count(output: std::process::Output) -> usize {
   // -1 to deal with newline at end of output
-  output.stdout.iter().filter(|&&i| i == b'\n').count() - 1
+//  output.stdout.iter().filter(|&&i| i == b'\n').count() - 1
+//}
+
+
+pub fn count(output: std::process::Output) -> usize {
+  let raw_list = String::from_utf8_lossy(&output.stdout);
+  let list: Vec<&str> = raw_list.split('\n').collect();
+  list.iter().count() - 1 // -1 to deal with newline at end of output
 }
