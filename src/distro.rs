bb4fbac0118c57b3cb131e8d3876376d8d7efb99
ValleyKnight <ValleyKnight@protonmail.com>
2023-09-14 15:42:25 -0400

    trim quotes that may be in distro name

--- src/distro.rs
+++ src/distro.rs
@@ -4,5 +4,6 @@ pub fn dist(path: &str) -> Result<String, Error> {
     let file = std::fs::File::open(path)?;
     let line: String = crate::shared_functions::line(file, 0); // Expects NAME= to be on first line
     let distro_vec: Vec<&str> = line.split('=').collect();
-    Ok(String::from(distro_vec[1]))
+    let distro = distro_vec[1].replace('"', "");
+    Ok(distro)
 }
